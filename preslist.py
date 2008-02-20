f1b622d586be585bc6dfb57b3ad553318c5087e6
Brad Landis <bradleelandis@gmail.com>
2008-02-20 18:53:27 +0000

    Change PresList to an inherited class of gtk.TreeView

--- preslist.py
+++ preslist.py
@@ -20,15 +20,15 @@ import gtk.gdk
 import gobject
 
 
-class PresList:
+class PresList(gtk.TreeView):
 	'''Manage the presentation list'''
-	def __init__(self, treeview):
-		self.pres_list_view = treeview
-		self.pres_list_view.set_size_request(165, 250)
+	def __init__(self):
+		gtk.TreeView.__init__(self)
+		self.set_size_request(165, 250)
 		
 		column1 = gtk.TreeViewColumn("Presentation", gtk.CellRendererText(), text=1)
 		column1.set_sort_column_id(1)
-		self.pres_list_view.append_column(column1)
+		self.append_column(column1)
 		#TODO(?) Icon to represent the type
 		#column2 = gtk.TreeViewColumn(" ", gtk.CellRendererPixbuf(), text=2)
 		#column2.set_sort_column_id(2)
@@ -38,10 +38,10 @@ class PresList:
 		self.pres_list = gtk.ListStore(gobject.TYPE_PYOBJECT, gobject.TYPE_STRING)
 		self.pres_list.set_sort_column_id(1, gtk.SORT_ASCENDING)
 		
-		self.pres_list_view.set_model(self.pres_list)
+		self.set_model(self.pres_list)
 	
 	def get_active_item(self):
-		(model, s_iter) = self.pres_list_view.get_selection().get_selected()
+		(model, s_iter) = self.get_selection().get_selected()
 		if(s_iter):
 			return model.get_value(s_iter, 0)
 		else:
@@ -51,7 +51,7 @@ class PresList:
 		self.pres_list.append((item, item.title))
 	
 	def remove(self, item):
-		model = self.pres_list_view.get_model()
+		model = self.get_model()
 		iter1 = model.get_iter_first()
 		while model.get_value(iter1, 0).filename != item.filename:
 			iter1 = model.iter_next(iter1)
@@ -61,9 +61,9 @@ class PresList:
 		#May need to pass in variable to be updated instead of just
 		# updating the selected item. Shouldn't take much processing
 		# to just update all items.
-		(model, s_iter) = self.pres_list_view.get_selection().get_selected()
+		(model, s_iter) = self.get_selection().get_selected()
 		self.pres_list.set(s_iter, 1, model.get_value(s_iter, 0).title)
 	
 	def has_selection(self):
-		return bool(self.pres_list_view.get_selection().count_selected_rows())
+		return bool(self.get_selection().count_selected_rows())
 
