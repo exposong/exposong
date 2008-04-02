15512205ba7fe7de73083e33dc245133e3aa0c9d
Brad Landis <bradleelandis@gmail.com>
2008-04-02 18:57:38 +0000

    Starting point for plugins.

---
+++ share/exposong/plugins/__init__.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python
+#
+#	Copyright (C) 2008 Fishhookweb.com
+#
+#	ExpoSong is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#	---
+
+"""
+This provides a base class for the plugin system.
+
+
+"""
+
+class Plugin(object):
+	'''
+	Custom plugins should inherit from this class.
+	'''
+	capabilities = []
+	
+	def __repr__(self):
+		return '<%s %r>' % (
+			self.__class__.__name__,
+			self.capabilities
+		)
+	
+	def get_version():
+		pass
+	
+	def get_description():
+		return ""
+
+
+def init_plugin_system(cfg):
+	'Load plugins dynamically.'
+	if not cfg['plugin_path'] in sys.path:
+		sys.path.insert(0, cfg['plugin_path'])
+	load_plugins(cfg['plugins'])
+	
+
+def load_plugins(plugins):
+	'Import plugins.'
+	for plugin in plugins:
+		__import__(plugin, None, None, [''])
+
+
+def find_plugins():
+	'Return all of the current available plugins.'
+	return Plugin.__subclasses__()
+
+
+def get_plugins_by_capability(capability):
+	'Return all plugins that have a capability.'
+	result = []
+	for plugin in Plugin.__subclasses__():
+		if capability in plugin.capabilities:
+			result.append(plugin)
+	return result
+
