b90a44069ad169e36233a198259ccc69712da46a
Brad Landis <bradleelandis@gmail.com>
2010-03-13 23:35:34 +0000

    Modify the DATA_PATH location to the user's home directory by default. It can be modified in defs.py for Portable installs, or if the user has another preference.

---
+++ defs.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+#
+# Copyright (C) 2008 Fishhookweb.com
+#
+# ExpoSong is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os.path
+
+# The variable DATA_PATH can be used to customize where the data will be
+# stored. If no value is specified, it defaults to ~/exposong/data
+#
+# The following example uses "data" directly under the program directory:
+#
+#DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),'data')
+#
+# Or you can use "~/Documents/exposong"
+# directory.
+#
+#DATA_PATH = os.path.join(os.path.expanduser('~'),'Documents','exposong')
+
--- share/exposong/lib/exposong/__init__.py
+++ share/exposong/lib/exposong/__init__.py
@@ -15,12 +15,34 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from os.path import abspath, dirname, join, pardir
+import os.path, os
+from os.path import abspath, dirname, join, pardir, expanduser
 
 SHARED_FILES = abspath(join(dirname(__file__), pardir, pardir))
 LOCALE_PATH = join(SHARED_FILES, 'i18n')
 RESOURCE_PATH = join(SHARED_FILES, 'res')
-DATA_PATH = abspath(join(SHARED_FILES, pardir, pardir, 'data'))
+
+defs = None
+try:
+  __defs = abspath(join(SHARED_FILES, pardir, pardir, "defs.py"))
+  import imp
+  defs = imp.load_source('defs', __defs)
+except IOError:
+  pass
+
+if hasattr(defs, "DATA_PATH") and isinstance(defs.DATA_PATH, str):
+  DATA_PATH = defs.DATA_PATH
+else:
+  DATA_PATH = join(expanduser("~"),"exposong","data")
+
+# Initialize the data directories. This assumes that if they exist, they are
+# either directories or symlinks. We might need to handle the case where they
+# could be files at a later point (TODO).
+if not os.path.exists(DATA_PATH):
+  os.makedirs(DATA_PATH)
+for folder in ('bg','pres','sched','image'):
+  if not os.path.exists(join(DATA_PATH, folder)):
+    os.mkdir(join(DATA_PATH,folder))
 
 
 import pygtk
